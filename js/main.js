6789a19a75637f8e9bde6d9adb2136c8f5f9f409
unknown <guilhermebueno6@gmail.com>
2019-07-16 15:27:16 -0400

    js drag n drop

--- js/main.js
+++ js/main.js
@@ -2,4 +2,46 @@
 
   console.log("hallo");
 
+
+  const piecesBoard = document.querySelector('.songIcons'),
+
+	dropZones = document.querySelectorAll('.Disc');
+
+let draggablePieces = piecesBoard.querySelectorAll("img");
+
+draggablePieces.forEach(piece => {
+		piece.addEventListener("dragstart", function(e) {
+			console.log('draggin...');
+
+			e.dataTransfer.setData("text/plain", this.id);
+
+		});
+	});
+
+
+dropZones.forEach(zone => {
+	zone.addEventListener("dragover", function(e) {
+		e.preventDefault();
+		console.log('dragged something over me');
+
+		if (this.childElementCount == 1) {
+		console.log('one-child family! Go away!!');
+		{return;}
+	}
+
+});
+
+zone.addEventListener("drop", function(e){
+	e.preventDefault(); // dont do your default behaviour instead do the following (console log)
+	console.log('you dropped something over me');
+
+	let draggedElement = e.dataTransfer.getData("text/plain");
+
+	console.log('you dragged: ',draggedElement);
+
+	e.target.appendChild(document.querySelector(`#${draggedElement}`));
+	//^Snaps to squares
+});
+});
+
 })();
