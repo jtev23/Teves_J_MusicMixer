337a470b8e8c050ad7b649bdc8ac92456d6f6c9e
unknown <guilhermebueno6@gmail.com>
2019-07-25 15:37:11 -0400

    First Song Audios Implemented, Changes to the document outline to have a Cleaner html file, used css rules inside svg for the DJ Table

--- js/main.js
+++ js/main.js
@@ -5,9 +5,32 @@
 
   const piecesBoard = document.querySelector('.songIcons'),
 
-	dropZones = document.querySelectorAll('.Disc');
+	dropZones = document.querySelectorAll('.dropZones');
 
-let draggablePieces = piecesBoard.querySelectorAll("img");
+//Creating Audios
+  let alienSound = document.createElement('audio');
+  alienSound.src = "audio/Song1/Arp1.wav";
+
+  let fluteSound = document.createElement('audio');
+  alienSound.src = "audio/Song1/Flute.wav";
+
+  let drumSound1 = document.createElement('audio');
+  drumSound1.src = "audio/Song1/Drums.wav";
+
+  let choirSound1 = document.createElement('audio');
+  choirSound1.src = "audio/Song1/Arp2.wav";
+
+  let bellSound1 = document.createElement('audio');
+  bellSound1.src = "audio/Song1/Pluck.wav";
+
+  let pianoSound1 = document.createElement('audio');
+  pianoSound1.src = "audio/Song1/Piano.wav";
+
+
+
+  //End of audios
+
+let draggablePieces = piecesBoard.querySelectorAll(".songIcons img");
 
 draggablePieces.forEach(piece => {
 		piece.addEventListener("dragstart", function(e) {
@@ -26,22 +49,61 @@ dropZones.forEach(zone => {
 
 		if (this.childElementCount == 1) {
 		console.log('one-child family! Go away!!');
-		{return;}
+		return;
 	}
 
 });
 
 zone.addEventListener("drop", function(e){
+
+
+
 	e.preventDefault(); // dont do your default behaviour instead do the following (console log)
 	console.log('you dropped something over me');
 
 	let draggedElement = e.dataTransfer.getData("text/plain");
 
+  //make audio play
+  if (draggedElement.id == "AlienSong1")
+
+{  document.body.appendChild(alienSound);
+  alienSound.play();}
+
+  else if (draggedElement.id == "DrumsSong1")
+
+  {document.body.appendChild(drumSound1);
+  drumSound1.play();}
+
+  else if (draggedElement.id == "FluteSong1")
+
+{  document.body.appendChild(fluteSound);
+  fluteSound.play();}
+
+  else if (draggedElement.id == "PianoSong1")
+
+  {document.body.appendChild(pianoSound1);
+  pianoSound1.play();}
+
+  else if (draggedElement.id == "BellSong1")
+
+  {document.body.appendChild(bellSound1);
+  bellSound1.play();}
+
+  else if (draggedElement.id == "ChoirSong1")
+
+  {document.body.appendChild(choirSound1);
+  choirSound1.play();}
+
+
+
 	console.log('you dragged: ',draggedElement);
 
 	e.target.appendChild(document.querySelector(`#${draggedElement}`));
 	//^Snaps to squares
+
+
 });
+
 });
 
 })();
